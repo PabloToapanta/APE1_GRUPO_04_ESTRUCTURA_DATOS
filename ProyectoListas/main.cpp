8d5d248a4d389ae2009527f7b5dd1f1221088bae
Pablo Toapanta <205024625+PabloToapanta@users.noreply.github.com>
2026-03-18 22:14:03 -0500

    Add delete, printHeadtoTail, printTailToHead features

--- ProyectoListas/main.cpp
+++ ProyectoListas/main.cpp
@@ -110,16 +110,96 @@ public:
     }
 
     void eliminar(T valor) {
-        // TODO Equipo 2: Lógica aquí
-    }
+    	
+    	//Si primero es nulo, entonces la lista esta vacia
+    	if(primero==0){
+    		cout <<"La lista esta vacia";
+    		return;
+		}else if(primero->info==valor){//Si el valor es el primero
+			if(primero->sig==0){//solo hay un nodo
+			delete primero;
+			primero=0; //liberamos memoria
+			ultimo=0;
+			}else{//Hay mas nodos
+			Nodo<T>* curr2 = primero ->sig; //Nodo auxiliar que apunte al elemento siguiente
+			primero->sig->ant=0;//reasignamos el anterior del nodo siguiente a cero
+			delete primero;//eliminamos el nodo primero
+			primero=curr2;//reasignamos el puntero primero
+			}
+		}else if(ultimo->info==valor){//Si el valor es el ultimo
+			Nodo<T> *curr3 = ultimo->ant;
+        	ultimo->ant->sig=0;
+        	delete ultimo;
+        	ultimo=curr3;
+    	}else{//El valor este en medio de la lista
+
+    		Nodo<T>* curr = primero;
+    		while(curr!= 0 ){
+    			if(curr->info == valor){
+    				Nodo<T>* prev = curr->ant;
+    				Nodo<T>* next = curr->sig;
+    				
+    				prev->sig=next;
+    				next->ant=prev;
+    				
+    				curr->sig=0;
+    				curr->ant=0;
+    				
+    				delete curr;
+    				return;
+				}
+				curr=curr->sig;
+			}
+
+				cout<<"El valor no existe en la lista"<<endl;
+				
+			
+		}
+	}
 
     void mostrarInicioAFin() {
-        // TODO Equipo 2: Lógica aquí usando 'sig'
+        // Verificamos si la lista está vacía
+        if (primero == 0) {
+            cout << "La lista esta vacia\n";
+            return;
+        }
+
+        Nodo<T>* curr = primero;
+        cout << "Inicio -> Fin: ";
+        
+        // Recorremos usando 'sig'
+        while (curr != 0) {
+            cout << curr->info;
+            if (curr->sig != 0) {
+                cout << " <-> "; // Separador visual para listas dobles
+            }
+            curr = curr->sig;
+        }
+        cout << endl;
     }
 
     void mostrarFinAInicio() {
-        // TODO Equipo 2: Lógica aquí usando 'ant' empezando desde 'ultimo'
+        // Verificamos si la lista está vacía
+        if (ultimo == 0) {
+            cout << "La lista esta vacia\n";
+            return;
+        }
+
+        Nodo<T>* curr = ultimo;
+        cout << "Fin -> Inicio: ";
+        
+        // Recorremos usando 'ant'
+        while (curr != 0) {
+            cout << curr->info;
+            if (curr->ant != 0) {
+                cout << " <-> "; // Separador visual para listas dobles
+            }
+            curr = curr->ant;
+        }
+        cout << endl;
     }
+    
+    
 
     void consultarPrimeroUltimo() {
         if (primero != 0 && ultimo != 0) {
